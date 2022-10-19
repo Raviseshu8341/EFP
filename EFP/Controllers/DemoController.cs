450d38dd5f6867e7c9c00608dd04424fe4713a5d
STS666-RAVISESHU T <STS666-RAVISESHU T@STS666L-RAVISHE>
2022-10-19 10:46:31 +0530

    Add project files.

---
+++ EFP/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using EFP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFP.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult HomeLogin()
+        {
+            return View();
+        }
+        public IActionResult Insert(Class s)
+        {
+            using (var entity = new sampleContext())
+            {
+                EFPT t = new EFPT();
+                t.Name = s.Name;
+                t.Age = s.Age;
+                t.Address = s.Address;
+                t.City = s.City;
+                t.Created_Time_Stamp = DateTime.Now;
+                t.Update_Time_Stamp = DateTime.Now;
+                entity.EFPT.Add(t);
+                entity.SaveChanges();
+            }
+            return View("HomeLogin");
+        }
+    }
+}
---
+++ EFP/Models/Class.cs
@@ -0,0 +1,14 @@
+﻿namespace EFP.Models
+{
+    public class Class
+    {
+        public int Student_id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public bool Is_Deleted { get; set; }
+        public System.DateTime Created_Time_Stamp { get; set; }
+        public System.DateTime Update_Time_Stamp { get; set; }
+    }
+}
